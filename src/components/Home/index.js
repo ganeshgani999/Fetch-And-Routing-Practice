7786c5f081dd2a378f6323ecf9c7b2bbfded385e
ganeshgani999 <sivaganesh1399705@gmail.com>
2023-12-25 14:50:30 +0000

    Fetch And Routing Practice

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import BlogList from '../BlogList'
+import UserInfo from '../UserInfo'
+
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <BlogList />
+    <UserInfo />
+  </div>
+)
+
+export default Home
